858d482d0aae4ac82de27675d845e8a98e9b9d72
Luca Leon Happel <lucahappel99@gmx.de>
2024-07-27 01:04:45 +0200

    add: use lazyvims preconfigured Lean4 settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,6 +37,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.haskell" },
+    { import = "lazyvim.plugins.extras.lang.lean" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
