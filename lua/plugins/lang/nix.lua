02be6541894fb3c567d446e721ad9b05b13c95fc
Luca Leon Happel <lucahappel99@gmx.de>
2024-09-16 22:39:00 +0200

    add: nixd

--- lua/plugins/lang/nix.lua
+++ lua/plugins/lang/nix.lua
@@ -8,4 +8,35 @@ return {
       -- ...
     },
   },
+  -- add nidx to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        nixd = {
+          cmd = { "nixd" },
+          settings = {
+            nixd = {
+              nixpkgs = {
+                expr = "import <nixpkgs> { }",
+              },
+              formatting = {
+                command = { "nixfmt" },
+              },
+              options = {
+                nixos = {
+                  expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+                },
+                home_manager = {
+                  expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
