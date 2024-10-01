618e23e9cdf64a640ac0be29843f6e0deaa2e727
Luca Leon Happel <luca.happel@hhu.de>
2024-10-01 11:39:58 +0200

    add: updated version

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,6 +19,53 @@ local function get_latex_equation()
   return nil
 end
 
+--- Render a LaTeX equation to an image using `mathimg`
+--- @param equation string
+--- @param start_row number
+--- @param start_col number
+--- @param end_row number
+--- @param end_col number
+local function render_equation(equation, start_row, start_col, end_row, end_col)
+  local api = require("image")
+  local image_id = "render_equation"
+  api:clear(image_id)
+  -- local imgfile = vim.fn.tempname() .. ".png"
+  local imgfile = "/tmp/equation.png"
+  local width = (end_col - start_col) * 8
+  local height = (end_row - start_row) * 14
+  -- replace all $ with \$
+  local escapedEquation = string.gsub(equation, "%$", "\\$")
+  local cmd = {
+    "mathimg",
+    '--packages="amsmath"',
+    "--png",
+    '--width="' .. width .. '"',
+    '--height="' .. height .. '"',
+    '--output="' .. imgfile .. '"',
+    '"' .. escapedEquation .. '"',
+  }
+  -- run mathimg to render the equation
+  local out = vim.fn.system(cmd)
+  print(table.concat(cmd, " "))
+  -- wait while the image is being rendered
+  while not vim.fn.filereadable(imgfile) do
+    vim.fn.system("sleep 0.1")
+  end
+  print("Image rendered at " .. imgfile)
+  print("content: " .. out)
+  -- print(out)
+  -- print(imgfile)
+  local image = api.from_file(imgfile, {
+    id = image_id,
+    width = width,
+    height = height,
+  })
+  image:render()
+end
+
 map({ "n", "v" }, "<localleader>r", function()
-  print(get_latex_equation())
+  local equation, start_row, start_col, end_row, end_col = get_latex_equation()
+  if equation then
+    render_equation(equation, start_row, start_col, end_row, end_col)
+  end
 end, { noremap = true, silent = true })
