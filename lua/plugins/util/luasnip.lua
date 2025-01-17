6648bad67873b4e5182bd2201275657b0613934b
Luca Leon Happel <lucahappel99@gmx.de>
2025-01-17 14:01:48 +0100

    fix: luasnip expansion

--- lua/plugins/util/luasnip.lua
+++ lua/plugins/util/luasnip.lua
@@ -5,8 +5,27 @@ return {
       "rafamadriz/friendly-snippets",
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
-        require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/lua/snippets" })
+        require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/lua/snippets" } })
+      end,
+      keys = function()
+        local ls = require("luasnip")
+
+        vim.keymap.set({ "i", "s" }, "<C-L>", function()
+          ls.jump(1)
+        end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-J>", function()
+          ls.jump(-1)
+        end, { silent = true })
+
+        vim.keymap.set({ "i", "s" }, "<C-E>", function()
+          if ls.choice_active() then
+            ls.change_choice(1)
+          end
+        end, { silent = true })
       end,
     },
   },
+  -- {
+  --   "evesdropper/luasnip-latex-snippets.nvim",
+  -- },
 }
