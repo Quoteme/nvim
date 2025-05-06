48059efa237d4a513e8341a104cc42880df8e87c
Luca Leon Happel <lucahappel99@gmx.de>
2025-05-06 12:58:39 +0200

    remove: cmakelint as mason dependency

--- lua/plugins/lang/cmake.lua
+++ lua/plugins/lang/cmake.lua
@@ -15,4 +15,16 @@ return {
     --   opts.capabilities.offsetEncoding = { "utf-16" }
     -- end,
   },
+  {
+    "mason.nvim",
+    opts = function(_, opts)
+      -- remove `cmakelint`
+      for i, v in ipairs(opts.ensure_installed) do
+        if v == "cmakelint" then
+          table.remove(opts.ensure_installed, i)
+          break
+        end
+      end
+    end,
+  },
 }
