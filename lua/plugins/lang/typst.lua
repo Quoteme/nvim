53e82f8921b77ee1b69c8ef395aa7df96afc398b
Luca Leon Happel <lucahappel99@gmx.de>
2025-08-30 18:44:19 +0200

    fix: ensure typst lsp stuff is installed using mason

--- lua/plugins/lang/typst.lua
+++ lua/plugins/lang/typst.lua
@@ -20,4 +20,13 @@ return {
       },
     },
   },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "typstyle",
+        "tinymist",
+      },
+    },
+  },
 }
