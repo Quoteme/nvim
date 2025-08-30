e32ff33e5c89866c32b31b8c5916a8091d23d489
Luca Leon Happel <lucahappel99@gmx.de>
2025-08-30 18:41:50 +0200

    add: typst config

---
+++ lua/plugins/lang/typst.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "chomosuke/typst-preview.nvim",
+    lazy = false, -- or ft = 'typst'
+    version = "1.*",
+    opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "typstyle")
+      table.insert(opts.ensure_installed, "tinymist")
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["typst"] = { "typstyle" },
+      },
+    },
+  },
+}
