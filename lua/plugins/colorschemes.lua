7975426b87fff7b56fa65e9a98a6e2f34ed0e770
Luca Leon Happel <lucahappel99@gmx.de>
2026-07-13 21:17:13 +0200

    fix: colorscheme not correctly detecting light/dark mode on linux

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,6 +2,19 @@ if vim.g.vscode then
   return {}
 else
   return {
+    {
+      "f-person/auto-dark-mode.nvim",
+      opts = {
+        set_dark_mode = function()
+          vim.api.nvim_set_option_value("background", "dark", {})
+        end,
+        set_light_mode = function()
+          vim.api.nvim_set_option_value("background", "light", {})
+        end,
+        update_interval = 3000,
+        fallback = "dark",
+      },
+    },
     -- add gruvbox
     -- { "ellisonleao/gruvbox.nvim", opts = {
     --   contrast = "hard",
